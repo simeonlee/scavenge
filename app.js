cf3fc55d2b08dc9a2f14cda99ae6671180245f18
Simeon Lee <simeonhlee@gmail.com>
2016-04-24 16:01:18 -0400

    test

--- app.js
+++ app.js
@@ -259,8 +259,8 @@ app.get('/data', function(req, res) {
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
