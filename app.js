285df218f130164ec87e84c4d0ff09f661bf6cc6
Simeon Lee <simeonhlee@gmail.com>
2016-04-26 19:34:56 -0400

    experimenting with insta thumbnail extraction

--- app.js
+++ app.js
@@ -194,8 +194,15 @@ var success = function (data) {
   dataJSON = JSON.parse(data);
   console.log(dataJSON);
 
-  // send data to client
-  io.sockets.emit('retrieved tweets', dataJSON);
+  var dataJSONplusInsta = addInstaImgURL(dataJSON);
+
+  
+
+
+  setTimeout(function(){
+    // send data to client
+    io.sockets.emit('retrieved tweets', dataJSONplusInsta);
+  },1000);
 
   // print tweet
   var text = dataJSON.statuses[0].text;
@@ -419,6 +426,108 @@ setTimeout(function(){
 
 
 
+
+
+
+
+
+
+
+var addInstaImgURL = function(dataJSON) {
+
+  for (var i = 0; i < dataJSON.length; i++) {
+
+    // extract individual tweet status object
+    var status = dataJSON.statuses[i];
+    
+    // extract text of tweet including t.co url
+    var tweetText = status.text;
+
+    // add instagram thumbnail url to datajson object before transmittal to client
+    status.instaImgURL = returnInstaImgURL(tweetText);
+
+  }
+  return dataJSON;
+}
+
+
+
+
+var returnInstaImgURL = function() {
+
+    // find the link in the text that starts with 'https://t.co/xxx'
+    var expression = /https?:\/\/t\.[a-z]{2,6}\/([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+    var regex = new RegExp(expression);
+    
+    if (text.match(regex)) {
+      
+      // set innerURL to that link in the text that links to some content
+      var innerURL = text.match(regex);
+    
+      // var innerURL is now a t.co url - need to transform using ajax call to 
+      // www.linkexpander.com/?url=https://t.co/xxx to find instagram url
+      var instaURL = expandT_coURL(innerURL,extractInstaURL);
+      console.log(instaURL);
+
+      return instaURL;
+
+    } else {
+      
+      var innerURL = null;
+      
+      var instaURL = null;
+      return null;
+
+    }
+    
+  }
+
+
+var expandT_coURL = function(innerURL,extractInstaURL) {
+  $.ajax({
+    type: 'GET',
+    url: 'http://www.linkexpander.com/?url='+innerURL,
+    cache: false,
+    dataType: 'json',
+    jsonp: false,
+    success: function (data) {
+      try {
+        var expandedURL = data;
+        var instaURL = extractInstaURL(expandedURL);
+        return instaURL;
+      } catch (err) {
+        console.log(err);
+        return null;
+      }
+    }
+
+  })
+
+}
+
+
+
+
+var extractInstaURL = function(expandedURL) {
+  // extract instagram pic from twitter shortlink
+  $.ajax({
+    type: 'GET',
+    url: 'http://api.instagram.com/oembed?callback=&url='+expandedURL,
+    cache: false,
+    dataType: 'json',
+    jsonp: false,
+    success: function (data) {
+      try {
+          var thumbnailURL = data.thumbnail_url;
+          console.log(thumbnailURL);
+          return thumbnailURL;
+      } catch (err) {
+          console.log(err);
+          return null;
+      }
+    }
+  });
+}
 
 
 
