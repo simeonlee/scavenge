6de623be7888867d257d3860887e8355ab0513b0
Simeon Lee <simeonhlee@gmail.com>
2016-08-23 15:35:32 -0700

    Add scavenge-react files

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import './styles/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.root')
+);
